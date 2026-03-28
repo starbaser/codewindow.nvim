5e20cce91cd849142d441c09018eb87281046354
starbased <s@starbased.net>
2026-03-27 21:12:12 -0700

    flake: migrate to multi-system (x86_64-linux, aarch64-linux)

--- flake.nix
+++ flake.nix
@@ -5,50 +5,64 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (nixpkgs) lib;
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: lib.genAttrs supportedSystems f;
+      perSystem = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-      codewindowPlugin = pkgs.vimUtils.buildVimPlugin {
-        name = "codewindow.nvim";
-        src = self;
-      };
+          codewindowPlugin = pkgs.vimUtils.buildVimPlugin {
+            name = "codewindow.nvim";
+            src = self;
+          };
 
-      treesitterWithLua = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.lua ]);
+          treesitterWithLua = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.lua ]);
 
-      testNvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
-        pkgs.neovimUtils.makeNeovimConfig {
-          plugins = [
-            { plugin = pkgs.vimPlugins.mini-nvim; }
-            { plugin = treesitterWithLua; }
-            { plugin = codewindowPlugin; }
-          ];
-          extraLuaPackages = ps: [ ps.tiktoken_core ];
-        }
-      );
+          testNvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
+            pkgs.neovimUtils.makeNeovimConfig {
+              plugins = [
+                { plugin = pkgs.vimPlugins.mini-nvim; }
+                { plugin = treesitterWithLua; }
+                { plugin = codewindowPlugin; }
+              ];
+              extraLuaPackages = ps: [ ps.tiktoken_core ];
+            }
+          );
+        in
+        {
+          packages = {
+            default = codewindowPlugin;
+          };
+
+          checks = {
+            smoke = pkgs.runCommand "codewindow-smoke" {} ''
+              export HOME=$(mktemp -d)
+              ${testNvim}/bin/nvim --headless -c 'lua vim.cmd("qa!")' 2>&1
+              touch $out
+            '';
+
+            tests = pkgs.runCommand "codewindow-tests" { nativeBuildInputs = [ pkgs.curl ]; } ''
+              export HOME=$(mktemp -d)
+              cd ${self}
+              ${testNvim}/bin/nvim --headless -u tests/minimal_init.lua \
+                -c "lua MiniTest.run({ collect = { find_files = function() \
+                  return vim.fn.globpath('tests', 'test_*.lua', true, true) \
+                end } })"
+              touch $out
+            '';
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              packages = [ testNvim pkgs.watchexec pkgs.stylua ];
+            };
+          };
+        });
     in
     {
-      packages.${system}.default = codewindowPlugin;
-
-      checks.${system} = {
-        smoke = pkgs.runCommand "codewindow-smoke" {} ''
-          export HOME=$(mktemp -d)
-          ${testNvim}/bin/nvim --headless -c 'lua vim.cmd("qa!")' 2>&1
-          touch $out
-        '';
-
-        tests = pkgs.runCommand "codewindow-tests" { nativeBuildInputs = [ pkgs.curl ]; } ''
-          export HOME=$(mktemp -d)
-          cd ${self}
-          ${testNvim}/bin/nvim --headless -u tests/minimal_init.lua \
-            -c "lua MiniTest.run({ collect = { find_files = function() \
-              return vim.fn.globpath('tests', 'test_*.lua', true, true) \
-            end } })"
-          touch $out
-        '';
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [ testNvim pkgs.watchexec pkgs.stylua ];
-      };
+      packages = lib.mapAttrs (_: v: v.packages) perSystem;
+      checks = lib.mapAttrs (_: v: v.checks) perSystem;
+      devShells = lib.mapAttrs (_: v: v.devShells) perSystem;
     };
 }
